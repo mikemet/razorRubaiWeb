d41e00ef2b304a2c61eb3fe95bcd1959b5cd0d89
mikemet <mikemet@yandex.ru>
2023-05-17 19:44:19 +0300

    Добавить README.md.

---
+++ Pages/Random.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorRubaiWeb.Pages
+{
+    public class RandomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
